bc5bef0b9e64cef21bec999be6c5e7d37c4d408c
Edrielle <edrielleduarte@gmail.com>
2022-01-12 20:24:35 -0300

    Portfólio - primeiro commit

---
+++ script.js
@@ -0,0 +1,63 @@
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks, navLinks2) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.navLinks2 = document.querySelectorAll(navLinks2);
+    this.activeClass = "active";
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+    this.navLinks2.forEach((item) => {
+      item.addEventListener("click", this.handleClick);
+    });
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li",
+  ".nav-list li a"
+);
+
+function typeWrite(elemento) {
+  const textoArray = elemento.innerHTML.split("");
+  elemento.innerHTML = " ";
+  textoArray.forEach(function (letra, i) {
+    setTimeout(function () {
+      elemento.innerHTML += letra;
+    }, 60 * i);
+  });
+}
+const lettermain = document.querySelector(".letter-main");
+
+lettermain.addEventListener("focus", ativar());
+
+function ativar() {
+  typeWrite(lettermain);
+}
